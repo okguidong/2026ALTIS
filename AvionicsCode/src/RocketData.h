eba3034e681bfa557d21e616814edad3caf6ff20
OkGuiDong <okd1031@gnu.ac.kr>
2025-12-30 11:54:31 +0900

    modified:   AvionicsCode/src/RocketData.h 	modified:   AvionicsCode/src/navigation.cpp

--- AvionicsCode/src/RocketData.h
+++ AvionicsCode/src/RocketData.h
@@ -7,6 +7,7 @@ struct __attribute__((packed)) SensorData {
     uint32_t timestamp = 0;   // 4 bytes
     float ax = 0.0f, ay = 0.0f, az = 0.0f;// 4 * 3 = 12 bytes
     float gx = 0.0f, gy = 0.0f, gz = 0.0f;// 4 * 3 = 12 bytes
+    float w = 0.0f, x = 0.0f, y = 0.0f, z = 0.0f;// 4 * 4 = 16 bytes
     float pressure = 0.0f;       // 4 bytes
     float raw_altitude = 0.0f;   // 4 bytes
     float filt_altitude = 0.0f;  // 4 bytes
@@ -16,6 +17,6 @@ struct __attribute__((packed)) SensorData {
     uint8_t ej2_state = 0; // 1 byte
     uint8_t sep_state = 0; // 1 byte
 }; 
-// 총 크기: 48 bytes
+// 총 크기: 64 bytes
 
 #endif
--- AvionicsCode/src/navigation.cpp
+++ AvionicsCode/src/navigation.cpp
@@ -16,16 +16,12 @@ void Navigation::update(SensorData &data, uint8_t sensor_update)
     applyFilter(data, sensor_update);
 
     // 1단계: 아직 발사 안 됨 (대기 상태)
-    if (!_launched)
+    if (data.flight_state == 0)
     {
-        data.flight_state = 0; // Ready
-        if (sensor_update & UPDATE_ACCEL)
-        {
 
-        }
     }
     // 2단계: 발사는 됐는데, 아직 분리는 안 됨 (상승 1단계)
-    else if (!_separated)
+    else if (data.flight_state == 1)
     {
         data.flight_state = 1; // Boost / Coast
         if (sensor_update & UPDATE_BARO)
@@ -34,16 +30,12 @@ void Navigation::update(SensorData &data, uint8_t sensor_update)
         }
     }
     // 3단계: 분리는 됐는데, 아직 사출은 안 됨 (상승 2단계 또는 활공)
-    else if (!_EJ1)
+    else if (data.flight_state == 2)
     {
-        data.flight_state = 2; // Apogee Check
-        if (sensor_update & UPDATE_BARO)
-        {
 
-        }
     }
     // 4단계: 1단부 사출까지 됨 (하강 상태)
-    else if (!_EJ2)
+    else if (data.flight_state == 3)
     {
         data.flight_state = 3; // Descent
         if (sensor_update & UPDATE_BARO)
